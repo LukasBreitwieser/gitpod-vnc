0ca826d30330285d539f9c12b0c7a74ee3e16146
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2021-10-25 14:21:23 +0200

    WIP startup

---
+++ open-jupyter-url.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+$GP_EXTERNAL_BROWSER "$(gp url 8888)/?token=$BDMGP_JUPYTER_TOKEN"
+
--- startup.sh
+++ startup.sh
@@ -2,7 +2,16 @@
 
 set -x
 
+BDM_SCRIPT_DIR=$(readlink -e $(dirname "${BASH_SOURCE[0]}"))
+
 cp -r $BDMSYS/notebook /workspace 
 cd /workspace 
+
+# open vnc window
 $GP_EXTERNAL_BROWSER $(gp url 6080)
-jupyter-notebook
+
+# jupyter notebooks
+export BDMGP_JUPYTER_TOKEN=$(echo $RANDOM | sha256sum | cut -c -64)
+jupyter-notebook \
+  --NotebookApp.token=$BDMGP_JUPYTER_TOKEN \
+  --browser="$BDM_SCRIPT_DIR/open-jupyter-url.sh %s"
